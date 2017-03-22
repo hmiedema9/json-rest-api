30e0ce560e2bef600628a9c51e5cb4ad3998ab47
Hayden Miedema <haydenmiedema@gmail.com>
2017-03-22 13:22:20 -0400

    Small update. Moved some things around

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 )
 
 func main() {
-
 	router := NewRouter()
 
 	log.Fatal(http.ListenAndServe(":8000", router))
--- router.go
+++ router.go
@@ -1,54 +1,18 @@
-/*
-	Class to serve as the router for a JSON REST API configured in Go
-	This uses the mux router from gorilla. In order to run, first install
-	the router with 'go get -u github.com/gorilla/mux'
-*/
 package main
 
-import (
-	"encoding/json"
-	"fmt"
-	"log"
-	"net/http"
+import "github.com/gorilla/mux"
 
-	"github.com/gorilla/mux"
-)
-
-type Language struct {
-	Name       string
-	Experience int
-	Difficulty int
-}
-
-type Languages []Language
-
-func main() {
+/* New Router - Function for main.go */
+func NewRouter() *mux.Router {
 
 	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/", Index)
-	router.HandleFunc("/Contact", Contact)
-	router.HandleFunc("/Languages/{languageId}", LanguageIndex)
-
-	log.Fatal(http.ListenAndServe(":8000", router))
-}
-
-func Index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Welcome!")
-}
-
-func Contact(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Contact Page!")
-}
-
-func LanguageIndex(w http.ResponseWriter, r *http.Request) {
-	// vars := mux.Vars(r)
-	// languageId := vars["languageId"]
-	// fmt.Fprintln(w, "Programming Language ID:", languageId)
-
-	languages := Languages{
-		Language{Name: "Go"},
-		Language{Name: "C#"},
+	for _, route := range routes {
+		router.
+			Methods(route.Method).
+			Path(route.Pattern).
+			Name(route.Name).
+			Handler(route.HandlerFunc)
 	}
 
-	json.NewEncoder(w).Encode(languages)
+	return router
 }
