b74fd27d085e9ada40e294bf3f576aec6d061808
Hayden Miedema <haydenmiedema@gmail.com>
2017-03-23 13:17:25 -0400

    Added views and index for testing/set up of auth down the road

--- src/main.go
+++ src/main.go
@@ -8,5 +8,11 @@ import (
 func main() {
 	router := NewRouter()
 
+	// On the default page we will simply serve our static index page.
+  	router.Handle("/", http.FileServer(http.Dir("./views/")))
+  	// We will setup our server so we can serve static assest like images, css from the /static/{file} route
+  	router.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
+
+
 	log.Fatal(http.ListenAndServe(":8000", router))
 }
